702bbd4b7307d6e66884ed7581972cf9ecfd9832
Ashebir unegnaw <52658863+ashu-mesi@users.noreply.github.com>
2024-07-01 20:39:40 +0300

    final  commit

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosBase = axios.create({
-    baseURL: 'http://localhost:5500/api'
+    // baseURL: 'http://localhost:5500/api'
+    baseURL: 'https://evangadi-forum-starter-1.onrender.com/api'
 })
 
 export default axiosBase
